4655bd3654d6ee6de8db20a99f4fdec923a1cfec
armyta23 <91170547+armyta23@users.noreply.github.com>
2021-12-29 03:35:44 +0700

    Add files via upload

---
+++ praktikum 9 elkom 1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 03:17:51 2021
+
+@author: LENOVO
+"""
+
+print('=========== ELKOM 1 ===========')
+list=[5,8,23,40,57,69]
+print(list)
+print('hasil reverse ke tuple')
+print(tuple(list))
---
+++ praktikum 9 elkom 3.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 29 03:26:02 2021
+
+@author: LENOVO
+"""
+
+def mengalikan(a):
+    taksiran=1
+    for n in list:
+        taksiran*=n
+    print(taksiran)
+    return taksiran
+
+list=[10,23,30,32]
+print(list)
+mengalikan(list)
